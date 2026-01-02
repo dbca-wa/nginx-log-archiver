9fdc6f88c8e4734ba931544210038c54d279c363
Ashley Felton <ashley@ropable.com>
2026-01-02 14:03:09 +0800

    Separate functions to download zone records from each source.

--- dns_zones.py
+++ dns_zones.py
@@ -3,9 +3,11 @@ import csv
 import json
 import os
 from tempfile import NamedTemporaryFile, TemporaryDirectory, TemporaryFile
+from typing import List, Tuple
 
 import crossplane
 import requests
+import tldextract
 from azure.cli.core import get_default_cli
 from dotenv import load_dotenv
 
@@ -89,7 +91,7 @@ def az_login(az_cli):
     return data
 
 
-def az_dns_zone_list(az_cli):
+def az_dns_zone_list(az_cli) -> List:
     outfile = TemporaryFile("r+")
     az_cli.invoke(["network", "dns", "zone", "list"], out_file=outfile)
     outfile.flush()
@@ -99,7 +101,7 @@ def az_dns_zone_list(az_cli):
     return data
 
 
-def az_dns_recordset_list(az_cli, zone_name: str, resource_group: str = "oim-appservices"):
+def az_dns_recordset_list(az_cli, zone_name: str, resource_group: str = "oim-appservices") -> List:
     outfile = TemporaryFile("r+")
     az_cli.invoke(["network", "dns", "record-set", "list", "--resource-group", resource_group, "--zone-name", zone_name], out_file=outfile)
     outfile.flush()
@@ -109,22 +111,24 @@ def az_dns_recordset_list(az_cli, zone_name: str, resource_group: str = "oim-app
     return data
 
 
-def get_all_zone_records():
-    # CLOUDFLARE
+def get_cloudflare_zone_records(zone_records: List) -> List[Tuple]:
     cf_api_token = os.getenv("CLOUDFLARE_API_TOKEN")
     cf_zones = cf_list_zones(cf_api_token)["result"]
     cf_zone_ids = []
+
     for zone in cf_zones:
         cf_zone_ids.append((zone["name"], zone["id"]))
 
-    zone_records = []
     for zone in cf_zone_ids:
-        LOGGER.info(f"Checking {zone[0]} (Cloudflare)")
+        LOGGER.info(f"Checking zone {zone[0]} (Cloudflare)")
         records = cf_list_dns_records(cf_api_token, zone[1])["result"]
         for record in records:
             zone_records.append((record["name"], record["content"], record["type"], "Cloudflare"))
 
-    # AZURE DNS
+    return zone_records
+
+
+def get_azure_zone_records(zone_records: List) -> List[Tuple]:
     az = get_default_cli()
     az_login(az)
     zones = az_dns_zone_list(az)
@@ -145,7 +149,10 @@ def get_all_zone_records():
         elif record["type"] == "Microsoft.Network/dnszones/CNAME":
             zone_records.append((record["fqdn"], record["CNAMERecord"]["cname"], record["type"], "Azure"))
 
-    # NGINX
+    return zone_records
+
+
+def get_nginx_zone_records(zone_records: List, include_redirect_rules: bool = True) -> List[Tuple]:
     repo_dest = TemporaryDirectory()
     cloned_repo = clone_repo(repo_dest.name, GITHUB_ACCESS_TOKEN, GITHUB_REPO)
     if cloned_repo:
@@ -182,27 +189,43 @@ def get_all_zone_records():
                 for target in targets:
                     zone_records.append((name[0], target[0], target[1], "Nginx"))
             else:
-                zone_records.append((name[0], "", "redirect", "Nginx"))
+                if include_redirect_rules:
+                    zone_records.append((name[0], "", "redirect", "Nginx"))
 
     return zone_records
 
 
-def parse_dns_zones(container_dest: str, blob_name: str):
+def get_all_zone_records(include_redirects: bool = True) -> List:
+    cloudflare_records = get_cloudflare_zone_records([])
+    azure_records = get_azure_zone_records([])
+    nginx_records = get_nginx_zone_records([], include_redirects)
+
+    return cloudflare_records + azure_records + nginx_records
+
+
+def parse_dns_zones(container_dest: str, blob_name: str, include_redirects: bool = True):
     LOGGER.info("Querying DNS records")
-    zone_records = get_all_zone_records()
+    zone_records = get_all_zone_records(include_redirects)
     zone_records = sorted(zone_records)
 
     # Write the parsed DNS records to a temporary file.
     out_csv = NamedTemporaryFile(mode="w", suffix=".csv")
     writer = csv.writer(out_csv)
-    writer.writerow(["SUBDOMAIN", "TARGET", "TYPE", "SOURCE"])
+    writer.writerow(["URL", "SUBDOMAIN", "DOMAIN", "TARGET", "TYPE", "SOURCE"])
 
     for record in zone_records:
-        # Skip TXT records.
-        if record[2] == "TXT":
+        # Skip certain classes of DNS record.
+        if record[2] in ["TXT", "MX", "SRV"]:
             continue
 
-        writer.writerow([record[0], record[1], record[2], record[3]])
+        # Strip any trailing fullstop character from the URL.
+        record = list(record)
+        url = record[0]
+        if url.endswith("."):
+            url = url[0:-1]
+
+        ext = tldextract.extract(url)
+        writer.writerow([url, ext.subdomain, ext.top_domain_under_public_suffix, record[1], record[2], record[3]])
 
     # Upload the temp file to blob storage.
     upload_log(out_csv.name, container_dest, CONN_STR, blob_name=blob_name)
@@ -226,8 +249,15 @@ if __name__ == "__main__":
         action="store",
         required=False,
     )
+    parser.add_argument(
+        "--exclude-redirects",
+        help="Exclude Nginx redirect rules from the output (optional)",
+        action="store_true",
+        required=False,
+    )
     args = parser.parse_args()
     parse_dns_zones(
         container_dest=args.container,
         blob_name=args.blob_name,
+        include_redirects=not args.include_redirects,
     )
